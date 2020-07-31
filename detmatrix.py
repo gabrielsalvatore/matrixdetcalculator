8d7bc4dfb4089088ce3c6f250ae7b17e5923f853
Gabriel Salvatore Xavier Saccoccio <62920284+gabrielgsx@users.noreply.github.com>
2020-07-31 11:39:43 -0300

    Add files via upload

---
+++ detmatrix.py
@@ -0,0 +1,16 @@
+lista = []
+for l in range(1, 4):
+    for c in range(1, 4):
+        matriz = int(input(f'\033[31mType a value [{l}, {c}] '))
+        lista.append(matriz)
+primeiro = (lista[0] * lista[4] * lista[8])
+segundo = (lista[1] * lista[5] * lista[6])
+terceiro = (lista[2] * lista[3] * lista[7])
+primeiro2 = (lista[2] * lista[4] * lista[6])
+segundo2 = (lista[0] * lista[5] * lista[7])
+terceiro2 = (lista[1] * lista[3] * lista[8])
+soma = ((primeiro + segundo + terceiro) - (primeiro2 + segundo2 + terceiro2))
+print(f'\033[34mThe result is =\033[m')
+for c in range(len(lista)):
+    print(f'[{lista[c] }]',end='\n' if c == 2 or c == 5 else "")
+print(f'\n\033[1;34mdet(a) =  \033[1;35m{soma}',end='')
